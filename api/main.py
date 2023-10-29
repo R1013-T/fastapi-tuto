3e668a28d9157e2ac0874ae76c3161ae88100be8
R1013-T <nosuke1013@icloud.com>
2023-10-29 19:43:36 +0900

    add routes

--- api/main.py
+++ api/main.py
@@ -1,22 +1,27 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+# from pydantic import BaseModel
+
+from api.routers import task, done
 
 app = FastAPI()
-
-class Item(BaseModel):
-  name: str
-  price: float
-  is_offer: bool = None
-
-@app.get("/hello")
-async def hello():
-  return {"message": "Hello World"}
+app.include_router(task.router)
+app.include_router(done.router)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-  return {"item_id ====>": item_id, "q ====>": q}
+# class Item(BaseModel):
+#   name: str
+#   price: float
+#   is_offer: bool = None
 
-@app.put("/items/{item_id}")
-def update_item(item_od: int, item: Item):
-  return {"item_name": item.name, "item_id": item.id}
+# @app.get("/hello")
+# async def hello():
+#   return {"message": "Hello World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#   return {"item_id ====>": item_id, "q ====>": q}
+
+# @app.put("/items/{item_id}")
+# def update_item(item_od: int, item: Item):
+#   return {"item_name": item.name, "item_id": item.id}
